f5de3c2c23cc50d745085eb72435be5fb498c32b
Maxim Kozhin <maximkozhin@yandex.ru>
2017-08-03 20:13:25 +0300

    Update AlertBundle.php

--- assets/AlertBundle.php
+++ assets/AlertBundle.php
@@ -4,10 +4,17 @@ namespace maximkozhin\alert\assets;
 
 use yii\web\AssetBundle;
 
+/**
+ * Class AlertBundle
+ * @package maximkozhin\alert\assets
+ * @author Maxim Kozhin
+ */
 class AlertBundle extends AssetBundle
 {
+    /** @var string Папка хранения ресурсов */
     public $sourcePath = '@maximkozhin/alert/dist';
 
+    /** @var array CSS-ресурсы */
     public $css = [
         'css/alert.css',
     ];
