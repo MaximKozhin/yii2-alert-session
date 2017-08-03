7a47932bb328b64dc3e93cc989906ed8b55742cf
Maxim Kozhin <maximkozhin@yandex.ru>
2017-08-03 20:13:59 +0300

    Update Session.php

--- components/Session.php
+++ components/Session.php
@@ -2,15 +2,33 @@
 
 namespace maximkozhin\alert\components;
 
+/**
+ * Class Session
+ * @package maximkozhin\alert\components
+ * @author Maxim Kozhin
+ */
 class Session extends \yii\web\Session
 {
+    /**
+     * @var string
+     */
     public $flashIndex = 'flash';
 
+    /**
+     * @param string $key
+     * @param bool $value
+     * @param bool $removeAfterAccess
+     */
     public function addFlash($key, $value = true, $removeAfterAccess = true)
     {
         parent::addFlash('flash', ['type' => $key, 'value'=>$value], $removeAfterAccess);
     }
 
+    /**
+     * @param string $key
+     * @param bool $value
+     * @param bool $removeAfterAccess
+     */
     public function setFlash($key, $value = true, $removeAfterAccess = true)
     {
         parent::addFlash('flash', ['type' => $key, 'value' => $value], $removeAfterAccess);
